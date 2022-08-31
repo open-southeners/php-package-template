309a1ae087867b4bbc9fa896dec6b7c73fb5dd5a
Ruben Robles <d8vjork@outlook.com>
2022-08-31 22:29:22 +0200

    add docblock to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,12 @@ use OpenSoutheners\PhpPackage\ServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string>
+     */
     protected function getPackageProviders($app)
     {
         return [
